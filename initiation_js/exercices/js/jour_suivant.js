91965e17e0059223c18b8a839a508cb5273dd394
2516e <mahleb.enzo@gmail.com>
2024-06-05 16:50:36 +0200

    05/06/24

---
+++ initiation_js/exercices/js/jour_suivant.js
@@ -0,0 +1,28 @@
+var jour = window.prompt("Saisissez un jour de la semaine :")
+var jour1 = "Lundi"
+var jour2 = "Mardi"
+var jour3 = "Mercredi"
+var jour4 = "Jeudi"
+var jour5 = "Vendredi"
+var jour6 = "Samedi"
+var jour7 = "Dimanche"
+var error = "jour inconnu"
+
+if (jour===jour1){
+    alert (jour2);
+} else if (jour===jour2){
+    alert (jour3)
+} else if (jour===jour3){
+    alert (jour4)
+} else if (jour===jour4){
+    alert (jour5)
+} else if (jour===jour5){
+    alert (jour6)
+} else if (jour===jour6){
+    alert (jour7)
+} else if (jour===jour7){
+    alert (jour1)
+} else {
+    alert (error)
+}
+
---
+++ initiation_js/exercices/js/permutation.js
@@ -0,0 +1,13 @@
+var a = 14
+var b = 26
+var c = 
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>" +"<br>")
+
+c = a
+a = b
+b = c
+document.write(a + "<br>");
+document.write(b + "<br>");
+document.write(c + "<br>" +"<br>")

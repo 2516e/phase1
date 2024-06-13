3d6e134aa136af941a1a3788a895f0e7b9d585cc
2516e <mahleb.enzo@gmail.com>
2024-06-13 16:53:54 +0200

    13.06.24

--- initiation_js/exercices/js/jours_mois.js
+++ initiation_js/exercices/js/jours_mois.js
@@ -1,6 +1,17 @@
-var nbMois = prompt("Chiffre correspondant au mois :")
+var nbMois = parseInt(prompt("Chiffre correspondant au mois :"))
+var moisPair = ["Février","Avril","Juin","Août","Octobre","Décembre"]
+var moisPairLength 
+var moisImpair = ["Janvier","Mars","Mai","Juillet","Septembre","Novembre"]
 
-while (!isNaN || nbmois>12){
-    nbMois = prompt("Saisie invalide, veuillez entrer une valeur valide");
-    
+
+while (isNaN(nbMois) || nbMois>12 || nbMois <= 0){
+    nbMois = parseInt(prompt("Saisie invalide, veuillez entrer une valeur valide"));
+} 
+if (nbMois % 2 == 1 || nbMois == 8){
+    alert("Ce mois contient 31 jours !")
+} else if (nbMois % 2 == 0 && nbMois != 2) {
+    alert("Ce mois contient 30 jours !")
+} else {
+    alert("Ce mois contient 28 jours !")
 }
+
